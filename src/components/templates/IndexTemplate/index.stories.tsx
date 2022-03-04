8cfa1fce8ca68c4446e0de64ac95c6088a674d7a
parkeunsik <qkrdmstlr3@naver.com>
2022-03-04 11:10:47 +0900

    [chore] storybook init
    - storybook 설치 및 설정파일 세팅
    - index template story파일 생성

---
+++ src/components/templates/IndexTemplate/index.stories.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import IndexTemplate from ".";
+
+export default {
+  title: "Templates/indexTemplate",
+  component: IndexTemplate,
+};
+
+export const indexTemplate = () => <IndexTemplate />;
