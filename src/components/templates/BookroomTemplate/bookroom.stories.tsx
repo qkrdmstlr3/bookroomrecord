36ed54bdcea3061c48ad60d97220d3e078d1dc90
parkeunsik <qkrdmstlr3@naver.com>
2022-03-07 16:51:56 +0900

    [test] bookroomTemplate story파일 구현
    - UI테스트 위해서 story파일을 구현

---
+++ src/components/templates/BookroomTemplate/bookroom.stories.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import BookroomTemplate from '.';
+
+export default {
+  title: 'Templates/bookroomTemplate',
+  component: BookroomTemplate,
+};
+
+export const bookroomTemplate = () => <BookroomTemplate />;
