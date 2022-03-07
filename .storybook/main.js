17c2f2e7c039780869820946e959a01704a1d936
parkeunsik <qkrdmstlr3@naver.com>
2022-03-07 17:20:18 +0900

    [chore] storybook에 gatsby addon추가
    - gatsby 내장함수 사용을 위한 addon추가
    - preview파일에서 경로 설정

--- .storybook/main.js
+++ .storybook/main.js
@@ -5,6 +5,7 @@ module.exports = {
     '@storybook/addon-essentials',
     '@storybook/addon-actions',
     '@storybook/addon-knobs',
+    'storybook-addon-gatsby',
   ],
   core: {
     builder: 'webpack5',
