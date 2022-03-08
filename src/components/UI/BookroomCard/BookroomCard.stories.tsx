e65c633261436edce8f214870af21d294f154c88
parkeunsik <qkrdmstlr3@naver.com>
2022-03-08 09:36:28 +0900

    [feat] BookroomCard UI Component 더미 구현
    - 책방카드를 표기하기 위한 컴포넌트를 구현하였다

---
+++ src/components/UI/BookroomCard/BookroomCard.stories.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import BookroomCard from '.';
+
+export default {
+  title: 'UI/BookroomCard',
+  component: BookroomCard,
+};
+
+export const bookroomCard = () => <BookroomCard />;
---
+++ src/components/UI/BookroomCard/index.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function BookroomCard() {
+  return <div>bookroom</div>;
+}
+
+export default BookroomCard;
