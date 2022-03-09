d9e6818d684e3bb4e74eb5ce149dcddc39678c6b
parkeunsik <qkrdmstlr3@naver.com>
2022-03-09 22:51:27 +0900

    [chore] gatsby markdown 설정
    - 마크다운 문서의 format정보를 가져올 수 있도록 플러그인 설치
    - bookroom page에서 graphql을 이용해서 필요한 마크다운 정보를 가져온다

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,9 +1,20 @@
 module.exports = {
+  siteMetadata: {
+    title: '책방록',
+  },
   plugins: [
+    `gatsby-transformer-remark`,
     {
       resolve: `gatsby-plugin-layout`,
       options: {
         component: require.resolve('./src/components/layouts/PageLayout/index.tsx'),
+      },
+    },
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        name: 'bookroom',
+        path: `${__dirname}/bookroom`,
       },
     },
   ],
--- src/pages/bookroom.tsx
+++ src/pages/bookroom.tsx
@@ -1,8 +1,40 @@
 import React from 'react';
+import { graphql, PageProps } from 'gatsby';
 import BookroomTemplate from '../components/templates/BookroomTemplate';
 
-function bookroom() {
+type DataProps = {
+  allMarkdownRemark: {
+    edges: {
+      node: {
+        frontmatter: {
+          title: string;
+          date: string;
+          img: string;
+        };
+      };
+    }[];
+  };
+};
+
+function bookroom({ data }: PageProps<DataProps>) {
+  console.log(data.allMarkdownRemark.edges.map((edge) => edge.node.frontmatter));
   return <BookroomTemplate />;
 }
+
+export const query = graphql`
+  query BookroomQuery {
+    allMarkdownRemark(sort: { order: ASC, fields: frontmatter___date }) {
+      edges {
+        node {
+          frontmatter {
+            title
+            date
+            img
+          }
+        }
+      }
+    }
+  }
+`;
 
 export default bookroom;
