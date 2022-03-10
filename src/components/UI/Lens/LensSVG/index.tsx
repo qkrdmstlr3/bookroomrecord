9b132fb65124c5a8344f15336cb535016a67750a
parkeunsik <qkrdmstlr3@naver.com>
2022-03-10 11:23:19 +0900

    [feat] Lens SVG Component 구현
    - 기존 svg파일을 없애고 컴포넌트로 구현
    - 로드해서 렌더링하는 방식에서 단순히 렌더링만 하는 것이 더 빠르다고 판단
      - 실제로 끊김없이 더 빠른 것 같다

---
+++ src/components/UI/Lens/LensSVG/index.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import * as Style from './styled';
+
+interface LensSVGProps {
+  anim: boolean;
+}
+
+function LensSVG({ anim }: LensSVGProps) {
+  return (
+    <Style.SVG xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100">
+      <defs>
+        <mask id="lens-mask" maskUnits="userSpaceOnUse">
+          <circle fill="white" r="49" cx="50" cy="50" />
+        </mask>
+      </defs>
+      <Style.Lens id="lens" r="49" cx="50" cy="50" />
+      <g mask="url(#lens-mask)">
+        <Style.FirstLine anim={anim} id="first-line" d="M -10 45.7 L 52.5 45.7" stroke="black" strokeWidth="1px" />
+        <Style.SecondLine anim={anim} id="second-line" d="M 47.5 54.3 L 110 54.3" stroke="black" strokeWidth="1px" />
+
+        <Style.ThirdLine anim={anim} id="third-line" d="M 47.5 45.7 L 10 100" stroke="black" strokeWidth="1px" />
+        <Style.FourthLine anim={anim} id="fourth-line" d="M 52.5 54.3 L 87 0" stroke="black" strokeWidth="1px" />
+
+        <Style.FifthLine anim={anim} id="fifth-line" d="M 45 50 L 79.2 100" stroke="black" strokeWidth="1px" />
+        <Style.SixthLine anim={anim} id="sixth-line" d="M 55 50 L 23.7 0" stroke="black" strokeWidth="1px" />
+      </g>
+    </Style.SVG>
+  );
+}
+
+export default LensSVG;
---
+++ src/components/UI/Lens/LensSVG/styled.ts
@@ -0,0 +1,107 @@
+import styled from '@emotion/styled';
+import { css, keyframes } from '@emotion/react';
+
+interface LineProps {
+  anim: boolean;
+}
+
+const firstLineAnim = keyframes`
+  0%, 20%, 100% { transform: translate(17.5px, -30.3px); }
+  40%, 80% { transform: none; }
+`;
+
+const secondLineAnim = keyframes`
+  0%, 20%, 100% { transform: translate(-17.5px, 30.3px); }
+  40%, 80% { transform: none; }
+`;
+
+const thirdLineAnim = keyframes`
+  0%, 20%, 100% { transform: translate(-17.5px, -30.3px); }
+  40%, 80% { transform: none; }
+`;
+
+const fourthLineAnim = keyframes`
+  0%, 20%, 100% { transform: translate(17.5px, 30.3px); }
+  40%, 80% { transform: none; }
+`;
+
+const fifthLineAnim = keyframes`
+  0%, 20%, 100% { transform: translate(-39px, 0); }
+  40%, 80% { transform: none; }
+`;
+
+const sixthLineAnim = keyframes`
+  0%, 20%, 100% { transform: translate(37px, 0); }
+  40%, 80% { transform: none; }
+`;
+
+export const SVG = styled.svg`
+  width: 100%;
+  height: 100%;
+`;
+
+export const Lens = styled.circle`
+  fill: white;
+  stroke: black;
+  stroke-width: 1px;
+`;
+
+export const FirstLine = styled.path<LineProps>`
+  transform: translate(17.5px, -30.3px);
+  animation: ${(prosp) =>
+    prosp.anim
+      ? css`
+          ${firstLineAnim} 2s linear infinite;
+        `
+      : 'none'};
+`;
+
+export const SecondLine = styled.path<LineProps>`
+  transform: translate(-17.5px, 30.3px);
+  animation: ${(prosp) =>
+    prosp.anim
+      ? css`
+          ${secondLineAnim} 2s linear infinite;
+        `
+      : 'none'};
+`;
+
+export const ThirdLine = styled.path<LineProps>`
+  transform: translate(-17.5px, -30.3px);
+  animation: ${(prosp) =>
+    prosp.anim
+      ? css`
+          ${thirdLineAnim} 2s linear infinite;
+        `
+      : 'none'};
+`;
+
+export const FourthLine = styled.path<LineProps>`
+  transform: translate(17.5px, 30.3px);
+  animation: ${(prosp) =>
+    prosp.anim
+      ? css`
+          ${fourthLineAnim} 2s linear infinite;
+        `
+      : 'none'};
+`;
+
+export const FifthLine = styled.path<LineProps>`
+  transform: translate(-39px, 0);
+  animation: ${(prosp) =>
+    prosp.anim
+      ? css`
+          ${fifthLineAnim} 2s linear infinite;
+        `
+      : 'none'};
+`;
+
+export const SixthLine = styled.path<LineProps>`
+  transform: translate(37px, 0);
+  animation: ${(prosp) =>
+    prosp.anim
+      ? css`
+          ${sixthLineAnim} 2s linear infinite;
+        `
+      : 'none'};
+`;
