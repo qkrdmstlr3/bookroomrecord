7f763b5540e65d8126709eb57f4868c6f80722c8
parkeunsik <qkrdmstlr3@naver.com>
2022-03-10 20:57:21 +0900

    [feat] mq 모듈 생성 및 BookroomCard에 적용
    - 모바일 대응위해 미디어쿼리 모듈 생성
    - BookroomCard에 생성한 모듈 적용
      - BookroomCard css를 화면에 맞게 변경
      - BookroomImage의 크기 변경

--- src/components/UI/BookroomCard/styled.ts
+++ src/components/UI/BookroomCard/styled.ts
@@ -1,24 +1,46 @@
 import styled from '@emotion/styled';
+import mq from '../../../style/mq';
 
 export const Container = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
+  justify-content: center;
   user-select: none;
+  width: min(40vw, 40vh);
+  height: min(40vw, 40vh);
 `;
 
 export const Address = styled.h2`
   font-size: 2.4rem;
   font-family: 'Noto Serif KR', serif;
+
+  ${mq('md')} {
+    margin: 0.2rem 0;
+    font-size: 1.8rem;
+  }
 `;
 
 export const Date = styled.span`
   margin: 1rem 0;
   font-size: 1.8rem;
   font-family: 'Noto Serif KR', serif;
+
+  ${mq('md')} {
+    font-size: 1.5rem;
+    margin: 0.2rem 0;
+  }
 `;
 
 export const Book = styled.span`
+  text-align: center;
+  display: inline-block;
+  width: 100%;
+  word-break: keep-all;
   font-family: 'Noto Serif KR', serif;
   font-size: 1.5rem;
+
+  ${mq('md')} {
+    font-size: 1rem;
+  }
 `;
--- src/components/UI/BookroomImage/styled.ts
+++ src/components/UI/BookroomImage/styled.ts
@@ -2,11 +2,11 @@ import styled from '@emotion/styled';
 
 export const Container = styled.div`
   width: 100%;
-  height: 100%;
+  height: 70%;
 `;
 
 export const Image = styled.img`
   width: 100%;
   height: 100%;
-  object-fit: cover;
+  object-fit: contain;
 `;
