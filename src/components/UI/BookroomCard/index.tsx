f4c04671c5ad27a6a517ace725bb3a0893f25afc
parkeunsik <qkrdmstlr3@naver.com>
2022-03-10 14:42:03 +0900

    [feat] BookroomCard Component 구현
    - 책방 카드 UI컴포넌트의 UI 구현

--- src/components/UI/BookroomCard/index.tsx
+++ src/components/UI/BookroomCard/index.tsx
@@ -1,7 +1,21 @@
 import React from 'react';
+import { BookroomType } from '../../templates/BookroomTemplate';
+import BookroomImage from '../BookroomImage';
+import * as Style from './styled';
 
-function BookroomCard() {
-  return <div>bookroom</div>;
+interface BookroomCardProps {
+  bookroom: BookroomType;
+}
+
+function BookroomCard({ bookroom }: BookroomCardProps) {
+  return (
+    <Style.Container>
+      <Style.Address>{bookroom.address}</Style.Address>
+      <BookroomImage src={bookroom.img} />
+      <Style.Date>{bookroom.date}</Style.Date>
+      <Style.Book>{bookroom.book}</Style.Book>
+    </Style.Container>
+  );
 }
 
 export default BookroomCard;
--- src/components/UI/BookroomCard/styled.ts
+++ src/components/UI/BookroomCard/styled.ts
@@ -1,3 +1,24 @@
 import styled from '@emotion/styled';
 
-export const Container = styled.div``;
+export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  user-select: none;
+`;
+
+export const Address = styled.h2`
+  font-size: 2.4rem;
+  font-family: 'Noto Serif KR', serif;
+`;
+
+export const Date = styled.span`
+  margin: 1rem 0;
+  font-size: 1.8rem;
+  font-family: 'Noto Serif KR', serif;
+`;
+
+export const Book = styled.span`
+  font-family: 'Noto Serif KR', serif;
+  font-size: 1.5rem;
+`;
